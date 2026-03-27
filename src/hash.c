bb971332d6aed5a8337c2a14fc0b45769448792a
tmaignien <tmaignien@bordeaux-inp.fr>
2026-03-27 16:17:29 +0100

    hash modifié

--- src/hash.c
+++ src/hash.c
@@ -5,8 +5,9 @@
 unsigned long hash (const char * line) {
     unsigned long hash = 5381;
     int c;
-    while ((c = *line++)) {
+    while ((c = *line) && c != '\n') {
         hash = ((hash << 5) + hash) + c;
+        line++;
     }
     return hash;
 }
@@ -28,9 +29,13 @@ void fix_collisions(const struct dfile_lines *f1, const struct dfile_lines *f2) 
     unsigned long counter = 1;
     for (int i = 0; i < f1->line_count; i++) {
         for (int j = 0; j < f2->line_count; j++) {
+            int len1 = f1->lines[i]->len;
+            int len2 = f2->lines[j]->len;
+            if (len1 > 0 && f1->lines[i]->content[len1 - 1] == '\n') len1--;
+            if (len2 > 0 && f2->lines[j]->content[len2 - 1] == '\n') len2--;
             if (f1->lines[i]->hash == f2->lines[j]->hash &&
-                (f1->lines[i]->len != f2->lines[j]->len ||
-                 strncmp(f1->lines[i]->content, f2->lines[j]->content, f1->lines[i]->len) != 0)) {
+                (len1 != len2 ||
+                 strncmp(f1->lines[i]->content, f2->lines[j]->content, len1) != 0)) {
                 // collision : on cherche une valeur libre dans f1 et dans f2 (jusqu'à j exclus)
                 while (hash_taken(counter, f1, f2, j)) counter++;
                 f2->lines[j]->hash = counter++;
