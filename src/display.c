b0f0acf5c57ef796db220fb526e9bd4dabdd1671
tmaignien <tmaignien@bordeaux-inp.fr>
2026-03-27 16:12:30 +0100

    couleur

--- src/display.c
+++ src/display.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+#define ANSI_RED_BG   "\033[48;2;80;0;0m"    // fond rouge foncé
+#define ANSI_GREEN_BG "\033[48;2;0;50;0m"    // fond vert foncé
+#define ANSI_RESET    "\033[0m"
+
 void final_display_diff(const struct dfile_lines *dfile1,
                         const struct dfile_lines *dfile2,
                         const char *script) {
@@ -20,14 +24,24 @@ void final_display_diff(const struct dfile_lines *dfile1,
         } else if (script[k] == 'i') {
             const char *line = dfile2->lines[count_i]->content;
             const int   len  = dfile2->lines[count_i]->len;
-            printf("+ %s", line);
-            if (len > 0 && line[len - 1] != '\n') printf("\n");
+            int print_len;
+            if (len > 0 && line[len - 1] == '\n') {
+                print_len = len - 1;
+            } else {
+                print_len = len;
+            }
+            printf(ANSI_GREEN_BG "+ %.*s" ANSI_RESET "\n", print_len, line);
             count_i++;
         } else if (script[k] == 'd') {
             const char *line = dfile1->lines[count_d]->content;
             const int   len  = dfile1->lines[count_d]->len;
-            printf("- %s", line);
-            if (len > 0 && line[len - 1] != '\n') printf("\n");
+            int print_len;
+            if (len > 0 && line[len - 1] == '\n') {
+                print_len = len - 1;
+            } else {
+                print_len = len;
+            }
+            printf(ANSI_RED_BG "- %.*s" ANSI_RESET "\n", print_len, line);
             count_d++;
         }
         k++;
